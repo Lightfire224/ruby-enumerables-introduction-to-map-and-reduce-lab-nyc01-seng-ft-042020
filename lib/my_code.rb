b3fec6d0916d77eaea15769844fe60852a6264c7
Elias Taveras <eliastave12@gmail.com>
2020-03-26 13:56:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,3 +58,9 @@ def reduce_to_all_true(source_array)
   end
   return true
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i <source_array.length do
+    if source_array[i] == False
+      return false
